8877eab44e3486bc436729d7089cf59035cb557a
andreeaNic <47979863+andreeaNic@users.noreply.github.com>
2020-03-03 23:06:42 +0200

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -7,14 +7,15 @@
 #include <set>
 #include <bits/stdc++.h>
 #include <tuple>
-
+/* Problem solved: almost same problem as 3. The only difference is that, here, all edges have their own penalty
+*/
 using namespace std;
 
 const int kNmax = 1005;
-
+//compare implemented from the priority queue we will used in the Dijkstra algorithm
 struct compare
 {
-    bool operator()(tuple <int,int, int> l, tuple <int,int, int> r)
+    bool operator()(tuple <int, int, int> l, tuple <int, int, int> r)
     {
         return (get<1> (l) ) > (get<1> (r) );
     }
@@ -32,7 +33,6 @@ class Task {
 	int n, m, t; 
 	vector<int>penalty;
 	vector<tuple<int, int, int> > adj[kNmax];
-	//vector<pair<int, int> > types[kNmax];
 
 	void read_input() {
 
@@ -43,87 +43,78 @@ class Task {
 		fin >> n >> m >> t;
 
 		for (i = 1; i <= m; i++) {
-            fin >> x >> y >> w >>type;
-            adj[x].push_back(make_tuple(y, w, type));
-            adj[y].push_back(make_tuple(x, w, type));
-        }
+		    fin >> x >> y >> w >>type;
+		    adj[x].push_back(make_tuple(y, w, type));
+		    adj[y].push_back(make_tuple(x, w, type));
+		}
 
-        penalty.resize(m + 1);
+		penalty.resize(m + 1);
 
-        for(i = 1; i <= t; i++){
-        	fin >> type_penalty;
-        	penalty[i] = type_penalty;
+		for(i = 1; i <= t; i++){
+			fin >> type_penalty;
+			penalty[i] = type_penalty;
 
-        }
+		}
 
-       fin.close();
+	       fin.close();
 
 	}
-
+//Dijkstra 
 	int get_result() {
 
 		int i,u;
-        vector<int> cost(n + 1, 0);
-        //printf("Costul muchiei spre adj_node este");
-        priority_queue< tuple<int, int, int>, vector< tuple <int, int, int> >, compare > q;
-        vector<int> vizited(n + 1, 0);
-        vector<int> penalty_applied(m + 1, 0);
+		vector<int> cost(n + 1, 0);
+		priority_queue< tuple<int, int, int>, vector< tuple <int, int, int> >, compare > q;
+		vector<int> vizited(n + 1, 0);
+		vector<int> penalty_applied(m + 1, 0);
 
-        int source = 1;
+		int source = 1;
+//Dijkstra
+		q.push(make_tuple(source, 0, 0));
+		cost[source] = 0;
+		vizited[source] = 1;
 
-        q.push(make_tuple(source, 0, 0));
-        cost[source] = 0;
-        vizited[source] = 1;
+		for (i = 1; i <= n; i++) {
+		    if (i != source) {
+			cost[i] = (1<<30);
+		    }
+		}
 
-        for (i = 1; i <= n; i++) {
-            if (i != source) {
-                cost[i] = (1<<30);
-            }
-        }
+		while (!q.empty()) {
+		    u = get <0> (q.top());
+			
+		    q.pop();
+		    vizited[u] = 1;
 
-        while (!q.empty()) {
-            u = get <0> (q.top());
+		    int type_edge_applied = get<2> (q.top());
 
-            printf("Nodul curent este:%d\n",u );
-            q.pop();
-            vizited[u] = 1;
+		    penalty_applied[type_edge_applied] = 1;
 
-            int type_edge_applied = get<2> (q.top());
+		    for (i = 0; i < adj[u].size(); i++) {
+			tuple<int, int, int> tup = adj[u][i];
+			int adj_node = get<0> (tup);
+			int cost_edge = get<1> (tup);
+			int type_edge = get<2> (tup);
 
-            //if(type_edge_applied <= penalty.size()){
-            penalty_applied[type_edge_applied] = 1;
-           // }
+			if(penalty_applied[type_edge] == 0){
+				cost_edge += penalty[type_edge];
+			}
 
-            for (i = 0; i < adj[u].size(); i++) {
-            	tuple<int, int, int> tup = adj[u][i];
-            	int adj_node = get<0> (tup);
-            	int cost_edge = get<1> (tup);
-            	int type_edge = get<2> (tup);
+			if(vizited[adj_node] == 0){
+				if(cost[adj_node] > cost[u] + cost_edge) {
+					cost[adj_node] = cost[u] + cost_edge;
+					q.push(make_tuple(adj_node, cost[adj_node], type_edge));
+				}
+			    } 
+		    }
+		}    
 
-            	printf("Tipul muchiei %d - %d este: %d \n", u, adj_node, type_edge);
 
-            	if(penalty_applied[type_edge] == 0){
-            		//&& penalty.size() >= type_edge){
-            		cost_edge += penalty[type_edge];
-            	}
+		if(vizited[n] == 0){
+			cost[n] = -1;
+		}
 
-                if(vizited[adj_node] == 0){
-	                if(cost[adj_node] > cost[u] + cost_edge) {
-		                cost[adj_node] = cost[u] + cost_edge;
-		                //cout << "Costul muchiei spre adj_node "<< adj_node<< "este "<< cost[adj_node]<<endl;
-		                printf("Costul muchiei spre adj_node %d este:%d \n", adj_node, cost[adj_node]);
-		                q.push(make_tuple(adj_node, cost[adj_node], type_edge));
-	               	}
-	            } 
-            }
-        }    
-        
-
-        if(vizited[n] == 0){
-        	cost[n] = -1;
-       	}
-        
-        return cost[n];
+		return cost[n];
 
 	}
 
